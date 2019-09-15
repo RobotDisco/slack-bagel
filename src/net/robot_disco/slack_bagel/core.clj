e88137649e616fd377310c0b90a49f297602e029
Gaelan D'costa <gdcosta@gmail.com>
2019-09-15 16:26:49 -0400

    Some documentation in code.

--- src/net/robot_disco/slack_bagel/core.clj
+++ src/net/robot_disco/slack_bagel/core.clj
@@ -7,31 +7,46 @@
             [clj-slack.rtm :as srtm]
             [clojure.string :as string]))
 
-(def token (env :bagel-slack-token))
-(def channel-name (env :bagel-channel-name))
+(def token
+  "The OAuth Access Token registered under the app this software will run as.
+
+  NOTE: This is the regular, non-bot `xoxp-`-prefixed token. The OAuth Bot Access Token does not work."
+  (env :bagel-slack-token))
+
+(def channel-name
+  "The Slack Channel this app will use for matching people."
+  (env :bagel-channel-name))
 
 (def connection {:api-url "https://slack.com/api"
                  :token token})
 
 (def my-user-id
+  ;; I can't find a better way to determine the app bot user's Slack User ID then by pulling it from a RTM connection initiation.
+  ;; In theory `bot.info` works but it never showed me any information when I tried it!
   (-> connection
       srtm/connect
       :self
       :id))
 
 (defn find-channel [name]
+  "Get the API object back for the Slack channel with the text identifier `name`"
   (->> (schan/list connection)
        :channels
        (filter #(= (:name %) name))
        first))
 
 (defn pair-users [name]
+  "Grab all users from slack channel named `name`, and randomly pair them.
+
+  This should not causee any issues if there are an odd-number of people in a channel, but one person won't be matched."
   (let [channel (find-channel name)
         members (:members channel)]
     (partition 2 (shuffle members))))
 
 (defn message-pairs [pairs]
+  "Given a sequence `pairs` of 2-element seqs containing two Slack User ID Strings, create a DM between them and tell them they've been matched."
   (doseq [pair pairs]
+    ;; We don't want to do this if we get a non two-tuple sequence ... which shouldn't be returned by `partition`, but just in case...
     (when (= (count pair) 2)
       (let [mpim (smpim/open connection (conj pair my-user-id))
             mpim-id (get-in mpim [:group :id])]
